900a4e929daa7eb4d4ad5ce702314ba3ce5e8fcc
Sherry Hsu <sherry.hsu@orionhealth.com>
2017-04-07 14:45:15 +1200

    Created first index

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 // First, index data: data.json from SitePoint and constituency.json from Compose
-const elasticClient = require('./connection.js').elasticClient;
-console.log(elasticClient);
+const client = require('./connection.js').elasticClient;
+
+//check connection health
+client.cluster.health({}, function(err, res, status) {
+  console.log("client health", res);
+});
 
 // import and index the data
+// each index created has specific setting associated e.g. no. shards and no. replicas
+// https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/api-reference.html#api-index
+// https://www.elastic.co/guide/en/elasticsearch/reference/current/indices.html
+
+//NOTE: Document> index API does not create a new index, it updates JSON in a specific index
+//Indices> Create API creates a new index
+client.indices.create({
+  index: 'gov'
+}, function(err, res){
+  if (err) {
+    console.error('ERROR creating index', err);
+  } else {
+    console.log('index created', res);
+  }
+});
+
 
 // constituencies.json
